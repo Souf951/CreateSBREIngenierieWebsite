58df58d9fff0fc55edad67743230365d1a202055
Souf951 <Soufiane.sbre@outlook.com>
2026-09-13 20:35:31 +0200

    Remove scroll video prototype route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -29,9 +29,6 @@ const ProjectMicroLogements = lazy(
 const ProjectVillaPrangins = lazy(
   () => import("./components/ProjectVillaPrangins"),
 );
-const ScrollVideoPrototype = lazy(
-  () => import("./components/ScrollVideoPrototype"),
-);
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -124,7 +121,6 @@ export default function App() {
           >
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/prototype-scroll" element={<ScrollVideoPrototype />} />
               <Route
                 path="/projet/tertiaire-geneve"
                 element={<ProjectTertiaire />}
