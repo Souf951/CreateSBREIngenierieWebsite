dd41de7706c88e65f44908b09371d595e2a19686
Souf951 <Soufiane.sbre@outlook.com>
2026-09-14 11:42:55 +0200

    Wire premium hover lift styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -46,6 +46,7 @@ import "../styles/home-copy-polish.css";
 import "../styles/case-study-video.css";
 import "../styles/projects-method-transition.css";
 import "../styles/partners-showcase.css";
+import "../styles/hover-lift.css";
 
 const ProjectTertiaire = lazy(() => import("./components/ProjectTertiaire"));
 const ProjectMicroLogements = lazy(
