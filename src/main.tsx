12317f64912272f22c0d28fe17c5452efcc1f4c7
Souf951 <Soufiane.sbre@outlook.com>
2026-09-17 19:34:43 +0200

    Enable premium partner form enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@
   import App from "./app/App";
   import "./app/partnerCollaborationEnhancer";
   import "./app/partnerValueSectionRemoval";
+  import "./app/partnerFormPremiumEnhancer";
   import "./styles/index.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
